9a1554dea81bfb3058002c0f4f7be43a2f363346
LAPTOP-5VCKOCNQ\user <skfhddlg@vascoenergy.com>
2025-08-24 19:20:36 +0900

    Naratteu.Linq.SelectWhen

---
+++ Naratteu.Linq/SelectWhen.cs
@@ -0,0 +1,17 @@
+﻿namespace Naratteu.Linq;
+
+public static class SelectWhen
+{
+    public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> selector)
+    {
+        foreach (var item in source)
+            if (selector(item) is (true, var value))
+                yield return value;
+    }
+    public static Pair<TResult> When<TResult>(bool condition, out TResult value)
+    {
+        System.Runtime.CompilerServices.Unsafe.SkipInit(out value);
+        return new(condition, value);
+    }
+    public record struct Pair<T>(bool Condition, T Value);
+}
