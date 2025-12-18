5172ae0f62c8dcac0f3bfd0d07e235fd945ea1c0
naratteu <skfhddlg@naver.com>
2025-12-19 08:59:26 +0900

    v0.0.2

---
+++ Naratteu.Linq/ArraySegmentExtensions.cs
@@ -0,0 +1,25 @@
+namespace Naratteu.Linq;
+
+public static class ArraySegmentExtensions
+{
+    public static ArraySegment<T> AsSegment<T>(this T[] arr) => arr;
+
+    public static IEnumerable<ArraySegment<T>> SegmentBy<T>(this ArraySegment<T> seg) where T : IEquatable<T>
+    {
+        for (int cnt; seg is [var fst, ..]; seg = seg.Slice(cnt))
+            yield return seg.Slice(0, cnt = 1 + seg.Slice(1).TakeWhile(fst.Equals).Count());
+    }
+    public static IEnumerable<ArraySegment<T>> SegmentBy<T, TT>(this ArraySegment<T> seg, Func<T, TT> match) where TT : IEquatable<TT>
+    {
+        for (int cnt; seg is [var fst, ..]; seg = seg.Slice(cnt))
+            yield return seg.Slice(0, cnt = 1 + seg.Slice(1).Select(match).TakeWhile(match(fst).Equals).Count());
+    }
+    public static IEnumerable<ArraySegment<T>> SegmentBy<T>(this ArraySegment<T> seg, Func<T, T, bool> match) => SegmentBy(seg, zip => match(zip.First, zip.Second));
+    public static IEnumerable<ArraySegment<T>> SegmentBy<T>(this ArraySegment<T> seg, Func<(T First, T Second), bool> match)
+    {
+        for (int cnt; seg is [_, _, ..]; seg = seg.Slice(cnt))
+            yield return seg.Slice(0, cnt = 1 + seg.Zip(seg.Slice(1)).TakeWhile(match).Count());
+        if (seg is not [])
+            yield return seg;
+    }
+}
--- Naratteu.Linq/WhereThen.gen.cs
+++ Naratteu.Linq/WhereThen.gen.cs
@@ -3,9 +3,9 @@ namespace Naratteu.Linq;
 partial class WhereThen
 {
     public static TResult First<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).First();
-    public static TResult FirstOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).FirstOrDefault();
+    public static TResult? FirstOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).FirstOrDefault();
     public static TResult Last<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).Last();
-    public static TResult LastOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).LastOrDefault();
+    public static TResult? LastOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).LastOrDefault();
     public static TResult Single<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).Single();
-    public static TResult SingleOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).SingleOrDefault();
+    public static TResult? SingleOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Pair<TResult>> predicate) => source.Where(predicate).SingleOrDefault();
 }
